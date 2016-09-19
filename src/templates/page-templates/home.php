12515b4cdf202067ab02b979d65823fec17d071d
laurent-muchacho <laurent.muchacho@ft.com>
2016-09-19 20:25:02 +0100

    style update

--- src/templates/page-templates/home.php
+++ src/templates/page-templates/home.php
@@ -18,37 +18,36 @@ get_header(); ?>
 
           <div class="o-grid-container primary-action">
             <div class="o-grid-row">
-              <div data-o-grid-colspan="6">
+              <div data-o-grid-colspan="6 S12">
                 <div class="chat">
                   <div>Welcome to Live FT Chat</div>
-                  <a href="#void" role="button" class="o-buttons">Ask a Question</a>
+                  <a href="#void" role="button" class="o-buttons o-buttons--big">Ask a Question</a>
                 </div>
               </div>
-              <div data-o-grid-colspan="6">
+              <div data-o-grid-colspan="6 S12">
                 <div class="contact">
                   <div>Contact our Customer Service</div>
-                  <a href="#void" role="button" class="o-buttons">Contact Us</a>
+                  <a href="/help/contact-us/" role="button" class="o-buttons o-buttons--big">Contact Us</a>
                 </div>
               </div>
             </div>
             <div class="o-grid-row">
-              <div data-o-grid-colspan="6">
+              <div data-o-grid-colspan="6 S12">
                 <div class="new-ft">
                   <div>Check out the new FT</div>
-                  <a href="#void" role="button" class="o-buttons">Take a Tour</a>
+                  <a href="#void" role="button" class="o-buttons o-buttons--big">Take a Tour</a>
                 </div>
               </div>
-              <div data-o-grid-colspan="6">
+              <div data-o-grid-colspan="6 S12">
                 <div class="old-ft">
                   <div>Not a big fan of the new look & feel?</div>
-                  <a href="#void" role="button" class="o-buttons">Return to the Old FT.com</a>
+                  <a href="#void" role="button" class="o-buttons o-buttons--big">Return to the Old FT.com</a>
                 </div>
               </div>
             </div>
           </div>
 
           <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
             <p><?php the_content(__('(more...)')); ?></p>
           <?php endwhile; else: ?>
             <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
